f036991a4cf970f25716e5806a27de2df8d91597
Ryan Hemmila <ryanhemmila@gmail.com>
2021-09-28 19:12:45 -0500

    f

--- mem_tb.sv
+++ mem_tb.sv
@@ -59,7 +59,7 @@ module mem_tb();
     mtc = new();
     for (int i = 0; i < DIM; i++) begin
       Arow = i;
-      Ain = mtc.A[i];
+      Ain[BITS_AB-1:0] = mtc.A[i][BITS_AB-1:0];
       @(posedge clk);
     end
 
@@ -70,7 +70,7 @@ module mem_tb();
     @(posedge clk);
 
     for (int i = DIM - 1; i >= 0; i--) begin
-      Bin = mtc.B[i];
+      Bin[BITS_AB-1:0] = mtc.B[i][BITS_AB-1:0];
       @(posedge clk);
     end
 
